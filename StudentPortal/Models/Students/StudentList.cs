531ddf32b384625c0b7ac76436ce30ab6f07195a
Kartikey45 <singhkartikey45@gmail.com>
2024-08-26 03:46:43 +0530

    MVC application setup.

---
+++ StudentPortal/Models/Students/StudentList.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentPortal.Models.Students
+{
+    public static class StudentList
+    {
+        public static List<Student>? StudentsList { get; set; }
+    }
+
+    public class Student
+    {
+        public Guid Id { get; set; } = new Guid();
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public bool Subscribed { get; set; }
+    }
+}
